01fdb5837126181bc63860233848723f433db929
svaja <smitavaja.uk@gmail.com>
2024-07-15 22:41:04 +0100

    Done

---
+++ src/main/java/com/gorest/constant/EndPoints.java
@@ -0,0 +1,11 @@
+package com.gorest.constant;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class EndPoints {
+    public static final String GET_ALL_USER = "/users";
+    public static final String GET_SINGLE_USER_BY_ID = "/{id}";
+    public static final String UPDATE_USER_BY_ID = "/{id}";
+    public static final String DELETE_USER_BY_ID = "/{id}";
+}
